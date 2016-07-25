936200f143dae2328e65609950cb14e7e209ad75
goosling <xiaoliu920@163.com>
2016-07-25 09:01:20 +0800

    ImageLoader

--- app/src/main/java/joe/com/androidexplore/crash/CrashHandler.java
+++ app/src/main/java/joe/com/androidexplore/crash/CrashHandler.java
@@ -1,7 +1,19 @@
 package joe.com.androidexplore.crash;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Environment;
+import android.util.Log;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by JOE on 2016/7/24.
@@ -32,11 +44,97 @@ public class CrashHandler implements Thread.UncaughtExceptionHandler {
     }
 
     public void init(Context context) {
-        
+        mDefaultCrashHandler = Thread.getDefaultUncaughtExceptionHandler();
+        Thread.setDefaultUncaughtExceptionHandler(this);
+        mContext = context.getApplicationContext();
     }
 
+    /**
+     * 最关键的函数，系统中存在未被捕获的异常，系统将会自动调用#uncaughtException
+     * @param thread
+     * @param ex
+     */
     @Override
     public void uncaughtException(Thread thread, Throwable ex) {
+        try{
+            //导出信息到sd卡中
+            dumpExceptionToSDCard(ex);
+            //上传异常信息到服务器
+            uploadExceptionToServer();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ex.printStackTrace();
+
+        //如果系统提供了默认的异常处理器，则交给系统去处理，否则就由自己结束自己
+        if(mDefaultCrashHandler != null) {
+            mDefaultCrashHandler.uncaughtException(thread, ex);
+        }else {
+            //Process.killProcess(Process.myPid());
+        }
+    }
+
+    private void dumpExceptionToSDCard(Throwable ex) throws IOException {
+        if(!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            if(DEBUG) {
+                Log.w(TAG, "sdcard unmounted, skip dump exception");
+                return;
+            }
+
+        }
+
+        File dir = new File(PATH);
+        if(!dir.exists()) {
+            dir.mkdirs();
+        }
+        long current = System.currentTimeMillis();
+        String time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(current));
+        File file = new File(PATH + FILE_NAME + time + FILE_NAME_SUFFIX);
+
+        try{
+            PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(file)));
+            pw.println(time);
+            dumpPhoneInfo(pw);
+            pw.println();
+            ex.printStackTrace(pw);
+            pw.close();
+        }catch (Exception e) {
+            Log.e(TAG, "dump crash info failed");
+        }
+    }
+
+    private void dumpPhoneInfo(PrintWriter pw) throws PackageManager.NameNotFoundException {
+        PackageManager pm = mContext.getPackageManager();
+        PackageInfo pi = pm.getPackageInfo(mContext.getPackageName(),
+                PackageManager.GET_ACTIVITIES);
+        pw.print("App versions: ");
+        pw.print(pi.versionName);
+        pw.print('_');
+        pw.print(pi.versionCode);
+
+        //Android版本号
+        pw.print("OS version: ");
+        pw.print(Build.VERSION.RELEASE);
+        pw.print("_");
+        pw.print(Build.VERSION.SDK_INT);
+
+        //手机制造商
+        pw.print("Vendor: ");
+        pw.print(Build.MANUFACTURER);
+
+
+        //手机型号
+        pw.print("Model: ");
+        pw.print(Build.MODEL);
+
+        //CPU架构
+        pw.print("CPU ABI: ");
+        pw.print(Build.CPU_ABI);
+
+    }
+
+    private void uploadExceptionToServer() {
 
     }
 }
